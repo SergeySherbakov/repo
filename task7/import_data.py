57fd2b9f4c3ceadb83e0b19e5f04809ab75fa37b
SergeySherbakov <serega.alexandr@yandex.ru>
2022-11-24 11:20:25 +0700

    task7

---
+++ task7/import_data.py
@@ -0,0 +1,10 @@
+# модуль импорта данных
+
+def import_data(data, sep=None):
+    with open('phone.csv', 'a+', encoding='UTF-8') as file:
+        if sep == None:
+            file.write(','.join(data))
+            file.write(f"\n")
+        else:
+            file.write(sep.join(data))
+            file.write(f"\n")
---
+++ task7/search_data.py
@@ -0,0 +1,22 @@
+# модуль поиска контакта
+
+from export_data import export_data
+from print_data import print_data
+
+
+def search_data(word, data):
+    if len(data) > 0:
+        for item in data:
+            if word in item:
+                return item
+    else:
+        return None
+
+
+def search_val(word, data, column):
+    if len(data) > 0:
+        for item in data:
+            if item != [] and item[int(column)] == word:
+                return item
+    else:
+        return None
