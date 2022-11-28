06d903b8075f191f94605d357b320f4650966f4d
SergeySherbakov <serega.alexandr@yandex.ru>
2022-11-28 13:37:40 +0700

    task9

---
+++ task9/import_data_csv.py
@@ -0,0 +1,13 @@
+import csv
+import export_data
+
+list = export_data.export_data()
+# print(type(list))
+print(list)
+
+with open('eggs.csv', 'w', newline='') as csvfile:
+
+    spamwriter = csv.writer(csvfile, delimiter=' ',
+                            quotechar='|', quoting=csv.QUOTE_MINIMAL)
+    for i in list:
+        spamwriter.writerow(i)
