d7d29bc93f0751a31a77a4aca20d2e734acd3e16
SergeySherbakov <serega.alexandr@yandex.ru>
2022-11-29 10:34:26 +0700

    task10

---
+++ task10/main.py
@@ -0,0 +1,14 @@
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+
+
+async def hello(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    await update.message.reply_text(f'Hello {update.effective_user.first_name}')
+
+
+app = ApplicationBuilder().token(
+    "5986622913:AAHaM26zJGf7qdweDIog_Vj71Kx3_eidFz0").build()
+
+app.add_handler(CommandHandler("hello", hello))
+
+app.run_polling()
